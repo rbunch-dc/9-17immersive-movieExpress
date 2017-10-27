81c1bfe18a1b6a35cf708a70cde0d31e868e4a9a
Robert Bunch <rob@digitalcrafts.com>
2017-10-27 15:41:00 -0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -30,13 +30,30 @@ router.post('/search', (req, res)=>{
 	// ANYTHING in a form that has a name snet through post 
 	// is available inside the req.body object
 	// req.query
-	res.json(req.body)
-})
+	// res.json(req.body)
+	var userSearchTerm = req.body.movieSearch;
+	var userSearchActor = req.body.actorSearch;
+	var queryString = req.query.key;
+	var searchUrl = `${apiBaseUrl}/search/movie?query=${userSearchTerm}&api_key=${config.apiKey}`; 
+	request.get(searchUrl,(error,response,movieData)=>{
+		var parsedData = JSON.parse(movieData);
+		// res.json(parsedData);
+		res.render('index',{ 
+			parsedData: parsedData.results,
+			imageBaseUrl: imageBaseUrl 
+		})
+		
+	})
+});
+
 // HAS NOTHING IN COMMON WITH THIS...
 router.get('/search', (req, res)=>{
 	// ANYTHING in a form that has a name sent through 
 	// a GET request, is availabel inside the req.query object
-	res.json(req.query)
+	// res.json(req.query)	
+	var userSearchTerm = req.query.movieSearch;
+	var userSearchActor = req.query.actorSearch;
+	res.send("Hey, this is the get route. Nobody is listening.")
 })
 
 router.get('/test', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+// var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
