ac5a1e0137edc47a2dc221bb4254f4c5f3bb44ea
Robert Bunch <rob@digitalcrafts.com>
2017-11-03 14:48:58 -0400

    Finish compareSync and login

--- routes/index.js
+++ routes/index.js
@@ -78,6 +78,33 @@ router.get('/login',(req, res)=>{
 router.post('/loginProcess', (req, res, next)=>{
 	var email = req.body.email;
 	var password = req.body.password;
+	// Check to see if the user is in the database...
+	var selectQuery = "SELECT * FROM users WHERE email = ?";
+	connection.query(selectQuery, [email], (error,results)=>{
+		if(results.length == 0){
+			// these aren't the droids we're looking for.
+			res.redirect('/login?msg=badUser');
+		}else{
+			// this email is in the database! Check to see if pass matches
+			// compareSync takes 2 args:
+			// 1. the english password
+			// 2. the hashed password in the DB we want to check against
+			var doTheyMatch = bcrypt.compareSync(password,results[0].password)
+			// compareSync retuns a bool
+			// if(1==2) = if (false)
+			// if(false){}
+			if(doTheyMatch === true){
+				// this is teh droid we're looking for. 
+				// We checked the English pass through bcrypt against the db hash
+				// and they matched.
+				res.redirect('/?msg=loggedIn')
+			}else{
+				// you dont want to sell me deathsticks.
+				// you want to go home and rethink life.
+				res.redirect('/login?msg=badpass')
+			}
+		}
+	})
 });
 
 router.post('/registerProcess', (req, res, next)=>{
